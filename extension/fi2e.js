85914c980ef139c4ae54987f0c865020820e0b26
Chris James Martin <cj@cjmart.in>
2014-10-22 16:05:36 -0700

    just inject the global script

--- extension/fi2e.js
+++ extension/fi2e.js
@@ -1,26 +1,9 @@
+//create a script element for the
+//embedder script
+var s = document.createElement('script'),
+	target =document.getElementsByTagName('script')[0];
 
-(function(win, doc, fe, script, url){
-	// If win[fe] exists, we'va already done this work, so do nothing.
-	if (!win[fe]) {
-		//if the embed code doesn't exist on the page already
-		//we create a queue
-		win[fe] = {
-			process: function(type){
-				//push any process calls into the queue
-				(win[fe].q=win[fe].q||[]).push(type);
-			}
-		};
-		//create a script element for the
-		//loader script
-		var s = doc.createElement(script),
-			target =doc.getElementsByTagName(script)[0];
-
-		s.async=1;
-		s.src=url;
-		//add the script to the DOM so that it loads
-		target.parentNode.insertBefore(s,target);
-	}
-})(window, document, 'FlickrEmbedr', 'script', 'https://widgets.flickr.com/embedr/embedr-loader.js');
-
-window['FlickrEmbedr'].process('all');
-console.log(window['FlickrEmbedr'].q);
+s.async=1;
+s.src='https://widgets.flickr.com/embedr/embedr-global.js';
+//add the script to the DOM so that it loads
+target.parentNode.insertBefore(s,target);
