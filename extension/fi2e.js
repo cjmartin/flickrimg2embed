5b4a7bcc48943303bc7c8e5eb281c2f503587d5e
Chris James Martin <cj@cjmart.in>
2014-10-22 15:32:45 -0700

    change loading scheme

--- extension/fi2e.js
+++ extension/fi2e.js
@@ -1,17 +1,25 @@
 
 (function(win, doc, fe, script, url){
-	//create a script element for the
-	//loader script
-	var s = doc.createElement(script),
-		target =doc.getElementsByTagName(script)[0];
+	// If win[fe] exists, we'va already done this work, so do nothing.
+	if (!win[fe]) {
+		//if the embed code doesn't exist on the page already
+		//we create a queue
+		win[fe] = {
+			process: function(type){
+				//push any process calls into the queue
+				(win[fe].q=win[fe].q||[]).push(type);
+			}
+		};
+		//create a script element for the
+		//loader script
+		var s = doc.createElement(script),
+			target =doc.getElementsByTagName(script)[0];
 
-	s.async=1;
-	s.src=url;
-	//add the script to the DOM so that it loads
-	target.parentNode.insertBefore(s,target);
+		s.async=1;
+		s.src=url;
+		//add the script to the DOM so that it loads
+		target.parentNode.insertBefore(s,target);
+	}
 })(window, document, 'FlickrEmbedr', 'script', 'https://widgets.flickr.com/embedr/embedr-loader.js');
 
-window.onload = function() {
-	window.FlickrEmbedr = new FlickrEmbedr();
-	window['FlickrEmbedr'].process('all');
-};
+window['FlickrEmbedr'].process('all');
